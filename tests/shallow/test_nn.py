6a103ec38a765375e1cffef34cc332ca8f30e6f4
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2025-01-23 00:09:51 +0800

    added tests and dev requirements

--- tests/shallow/test_nn.py
+++ tests/shallow/test_nn.py
@@ -1,16 +1,33 @@
 import pytest
 import torch
+
 from template_nn.shallow.fnn import F_NN
+
 
 @pytest.mark.parametrize("input_size, expected_output_shape", [
     (10, (1, 2)),
     (20, (1, 2)),
     (50, (1, 2)),
 ])
-def test_variable_input_size(input_size, expected_output_shape):
-
+def test_variable_input_size_shallow(input_size, expected_output_shape):
     # input_size - 5 - 2 Neural Network
-    model = F_NN(input_size=input_size, hidden_size=5, output_size=2)
+    model = F_NN(input_size, output_size=2, hidden_layer_num=1, hidden_sizes=[5])
+
+    x = torch.randn(1, input_size)
+
+    output = model(x)
+
+    assert output.shape == expected_output_shape, f"Expected output shape {expected_output_shape}, but got {output.shape}"
+
+
+@pytest.mark.parametrize("input_size, expected_output_shape", [
+    (10, (1, 2)),
+    (20, (1, 2)),
+    (50, (1, 2)),
+])
+def test_variable_input_size_deep(input_size, expected_output_shape):
+    # input_size - 5 - 2 Neural Network
+    model = F_NN(input_size, output_size=2, hidden_layer_num=5, hidden_sizes=[5 for _ in range (5)])
 
     x = torch.randn(1, input_size)
 
