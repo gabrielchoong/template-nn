ba191059c26ccd016ca1cd94f83bda22901913d3
Gabriel Choong Ge Liang <gabrielchoonggeliang@gmail.com>
2025-06-27 16:22:16 +0800

    increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="template-nn",
-    version="0.1.5.dev0",
+    version="0.1.6.dev0",
     packages=find_packages(where="src",
                            include=["template_nn", "template_nn.*"]),
     package_dir={"": "src"},
