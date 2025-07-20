5743472d247b47b852310f9f46801f0964396f6c
Gabriel Choong Ge Liang <gabrielchoonggeliang@gmail.com>
2025-07-20 13:34:29 +0800

    added option to add pooling layers manually instead of having it created during class creation

--- src/template_nn/networks/cml.py
+++ src/template_nn/networks/cml.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 import torch.nn as nn
 
 from ..args_val import (is_iterable, is_positive_int)
@@ -12,10 +12,13 @@ class CML(BaseNetwork):
     def __init__(self, tabular, visualise):
         super().__init__(tabular, get_model_keys("CML"), visualise)
 
-    def _build_model(self,
-                     conv_channels: List[int],
-                     conv_kernel_size: int = 3,
-                     pool_kernel_size: int = 3) -> nn.Sequential:
+    def _build_model(
+        self,
+        conv_channels: List[int],
+        conv_kernel_size: int = 3,
+        pool_kernel_size: int = 3,
+        pooling_indices: List[int] | None = None
+    ) -> nn.Sequential:
 
         is_positive_int(conv_kernel_size)
         is_positive_int(pool_kernel_size)
@@ -24,27 +27,40 @@ class CML(BaseNetwork):
         for channel_sizes in conv_channels:
             is_positive_int(channel_sizes)
 
-        return nn.Sequential(*self._create_layers(
-            conv_channels, conv_kernel_size, pool_kernel_size))
+        return nn.Sequential(
+            *self._create_layers(conv_channels, conv_kernel_size, pool_kernel_size, pooling_indices)
+        )
 
-    def _create_layers(self,
-                       conv_channels: List[int],
-                       conv_kernel_size: int = 3,
-                       pool_kernel_size: int = 3) -> List[nn.Module]:
-
+    def _create_layers(
+        self,
+        conv_channels: List[int],
+        conv_kernel_size: int = 3,
+        pool_kernel_size: int = 3,
+        pooling_indices: List[int] | None = None,
+    ) -> List[nn.Module]:
         layers = []
 
         in_size = conv_channels[0]
 
-        for out_size in conv_channels[1:]:
+        pooling_indices_set: Set[int] = set(pooling_indices or [])
+
+        for index, out_size in enumerate(conv_channels[1:]):
             layers += [
                 nn.Conv2d(
                     in_channels=in_size,
                     out_channels=out_size,
                     kernel_size=conv_kernel_size,
+                    padding=conv_kernel_size // 2,
                 ),
-                nn.MaxPool2d(kernel_size=pool_kernel_size, stride=2)
             ]
+
+            if index in pooling_indices_set:
+                layers += [
+                    nn.MaxPool2d(
+                        kernel_size=pool_kernel_size,
+                        stride=2,
+                    )
+                ]
 
             in_size = out_size
 
--- src/template_nn/networks/cnn.py
+++ src/template_nn/networks/cnn.py
@@ -15,9 +15,7 @@ class CNN(BaseNetwork):
     A Convolutional Neural Network (CNN) model for supervised learning.
     """
 
-    def __init__(self,
-                 tabular: dict | pd.DataFrame,
-                 visualise: bool = False) -> None:
+    def __init__(self, tabular: dict | pd.DataFrame, visualise: bool = False) -> None:
         """
         Class description goes here
         :param tabular: An input accepting both a dictionary or a pandas.DataFrame object.
@@ -27,74 +25,86 @@ class CNN(BaseNetwork):
         super().__init__(tabular, get_model_keys("CNN"), visualise)
         self.image_size = (0, 0)
 
-    def _build_model(self, image_size: tuple[int, int],
-                     conv_channels: List[int], conv_kernel_size: int,
-                     pool_kernel_size: int, fcn_hidden_sizes: List[int],
-                     activation_functions: List[nn.Module],
-                     output_channel: int) -> nn.Sequential:
-
+    def _build_model(
+        self,
+        image_size: tuple[int, int],
+        conv_channels: List[int],
+        conv_kernel_size: int,
+        pool_kernel_size: int,
+        pooling_indices: List[int],
+        fcn_hidden_sizes: List[int],
+        activation_functions: List[nn.Module],
+        output_channel: int,
+    ) -> nn.Sequential:
         self.image_size = image_size
         is_iterable(self.image_size)
 
         for sizes in self.image_size:
             is_positive_int(sizes)
 
-        return nn.Sequential(*self._create_layers(
-            conv_channels, conv_kernel_size, pool_kernel_size,
-            fcn_hidden_sizes, activation_functions, output_channel))
+        return nn.Sequential(
+            *self._create_layers(
+                conv_channels,
+                conv_kernel_size,
+                pool_kernel_size,
+                pooling_indices,
+                fcn_hidden_sizes,
+                activation_functions,
+                output_channel,
+            )
+        )
 
-    def _create_layers(self, conv_channels: List[int], conv_kernel_size: int,
-                       pool_kernel_size: int, fcn_hidden_sizes: List[int],
-                       activation_functions: List[nn.Module],
-                       output_channel: int) -> List[nn.Module]:
-
+    def _create_layers(
+        self,
+        conv_channels: List[int],
+        conv_kernel_size: int,
+        pool_kernel_size: int,
+        pooling_indices: List[int],
+        fcn_hidden_sizes: List[int],
+        activation_functions: List[nn.Module],
+        output_channel: int,
+    ) -> List[nn.Module]:
         height, width = self.image_size
 
         for _ in range(len(conv_channels) - 1):
-            height, width = self._compute_output_dim(height, width,
-                                                     conv_kernel_size,
-                                                     pool_kernel_size)
+            height, width = self._compute_output_dim(
+                height, width, conv_kernel_size, pool_kernel_size
+            )
 
         conv_layers = CML(
             {
                 "conv_channels": conv_channels,
                 "conv_kernel_size": conv_kernel_size,
-                "pool_kernel_size": pool_kernel_size
+                "pool_kernel_size": pool_kernel_size,
+                "pooling_indices": pooling_indices,
             },
-            visualise=False)
+            visualise=False,
+        )
 
         fcn_layers = FNN(
             {
                 "input_size": conv_channels[-1] * height * width,  # temp
                 "hidden_sizes": fcn_hidden_sizes,
                 "output_size": output_channel,
-                "activation_functions": activation_functions
+                "activation_functions": activation_functions,
             },
-            visualise=False)
+            visualise=False,
+        )
 
         return [conv_layers, nn.Flatten(), fcn_layers]
 
-    def _compute_dim(self,
-                     in_dim: int,
-                     kernel_size: int,
-                     stride=1,
-                     padding=0,
-                     dilation=1) -> int:
-        return (in_dim + 2 * padding - dilation *
-                (kernel_size - 1) - 1) // stride + 1
+    def _compute_dim(
+        self, in_dim: int, kernel_size: int, stride=1, padding=0, dilation=1
+    ) -> int:
+        return (in_dim + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
 
-    def _compute_output_dim(self, height: int, width: int,
-                            conv_kernel_size: int,
-                            pool_kernel_size: int) -> tuple[int, int]:
-
+    def _compute_output_dim(
+        self, height: int, width: int, conv_kernel_size: int, pool_kernel_size: int
+    ) -> tuple[int, int]:
         height = self._compute_dim(height, conv_kernel_size)
         width = self._compute_dim(width, conv_kernel_size)
 
-        height = self._compute_dim(height,
-                                   pool_kernel_size,
-                                   stride=pool_kernel_size)
-        width = self._compute_dim(width,
-                                  pool_kernel_size,
-                                  stride=pool_kernel_size)
+        height = self._compute_dim(height, pool_kernel_size, stride=pool_kernel_size)
+        width = self._compute_dim(width, pool_kernel_size, stride=pool_kernel_size)
 
         return (height, width)
