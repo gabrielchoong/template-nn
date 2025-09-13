229e5f443d66074c00a2e266eb1c5be7f1606916
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2025-09-13 16:13:28 +0800

    Refactored neural network classes with consistent structure

--- src/template_nn/networks/cnn.py
+++ src/template_nn/networks/cnn.py
@@ -1,5 +1,3 @@
-from typing import List
-import pandas as pd
 import torch.nn as nn
 
 from ..args_val import is_iterable, is_positive_int
@@ -15,86 +13,97 @@ class CNN(BaseNetwork):
     A Convolutional Neural Network (CNN) model for supervised learning.
     """
 
-    def __init__(self,
-                 tabular: dict | pd.DataFrame,
-                 visualise: bool = False) -> None:
+    def __init__(
+        self,
+        model_config: dict[str, int | list[int] | list[str]],
+        visualise: bool = False,
+    ) -> None:
         """
-        Class description goes here
-        :param tabular: An input accepting both a dictionary or a pandas.DataFrame object.
-        :param visualise: A toggle switch to visualize the model. OFF(False) by default.
+        :params model_config: A dictionary / json-like structure for model configuration
+        :params visualise: A boolean type for visualising structure. Default (False).
         """
-
-        super().__init__(tabular, get_model_keys("CNN"), visualise)
+        super().__init__(visualise)
+        self.model_keys = get_model_keys("CNN")
+        self.params = self._get_params(model_config, self.model_keys)
+        self.model = self._build_model(*self.params)
         self.image_size = (0, 0)
 
-    def _build_model(self, image_size: tuple[int, int],
-                     conv_channels: List[int], conv_kernel_size: int,
-                     pool_kernel_size: int, fcn_hidden_sizes: List[int],
-                     activation_functions: List[nn.Module],
-                     output_channel: int) -> nn.Sequential:
-
+    def _build_model(
+        self,
+        image_size: tuple[int, int],
+        conv_channels: list[int],
+        conv_kernel_size: int,
+        pool_kernel_size: int,
+        fcn_hidden_sizes: list[int],
+        activation_functions: list[str],
+        output_channel: int,
+    ) -> nn.Sequential:
         self.image_size = image_size
         is_iterable(self.image_size)
 
         for sizes in self.image_size:
             is_positive_int(sizes)
 
-        return nn.Sequential(*self._create_layers(
-            conv_channels, conv_kernel_size, pool_kernel_size,
-            fcn_hidden_sizes, activation_functions, output_channel))
+        return nn.Sequential(
+            *self._create_layers(
+                conv_channels,
+                conv_kernel_size,
+                pool_kernel_size,
+                fcn_hidden_sizes,
+                activation_functions,
+                output_channel,
+            )
+        )
 
-    def _create_layers(self, conv_channels: List[int], conv_kernel_size: int,
-                       pool_kernel_size: int, fcn_hidden_sizes: List[int],
-                       activation_functions: List[nn.Module],
-                       output_channel: int) -> List[nn.Module]:
-
+    def _create_layers(
+        self,
+        conv_channels: list[int],
+        conv_kernel_size: int,
+        pool_kernel_size: int,
+        fcn_hidden_sizes: list[int],
+        activation_functions: list[str],
+        output_channel: int,
+    ) -> list[nn.Module]:
         height, width = self.image_size
 
         for _ in range(len(conv_channels) - 1):
-            height, width = self._compute_output_dim(height, width,
-                                                     conv_kernel_size,
-                                                     pool_kernel_size)
+            height, width = self._compute_output_dim(
+                height, width, conv_kernel_size, pool_kernel_size
+            )
 
         conv_layers = CML(
             {
                 "conv_channels": conv_channels,
                 "conv_kernel_size": conv_kernel_size,
-                "pool_kernel_size": pool_kernel_size
+                "pool_kernel_size": pool_kernel_size,
             },
-            visualise=False)
+            visualise=False,
+        )
 
         fcn_layers = FNN(
             {
                 "input_size": conv_channels[-1] * height * width,  # temp
                 "hidden_sizes": fcn_hidden_sizes,
                 "output_size": output_channel,
-                "activation_functions": activation_functions
+                "activation_functions": activation_functions,
             },
-            visualise=False)
+            visualise=False,
+        )
 
         return [conv_layers, nn.Flatten(), fcn_layers]
 
-    def _compute_dim(self,
-                     in_dim: int,
-                     kernel_size: int,
-                     stride=1,
-                     padding=0,
-                     dilation=1) -> int:
-        return (in_dim + 2 * padding - dilation *
-                (kernel_size - 1) - 1) // stride + 1
+    def _compute_dim(
+        self, in_dim: int, kernel_size: int, stride=1, padding=0, dilation=1
+    ) -> int:
+        return (in_dim + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
 
-    def _compute_output_dim(self, height: int, width: int,
-                            conv_kernel_size: int,
-                            pool_kernel_size: int) -> tuple[int, int]:
-
+    def _compute_output_dim(
+        self, height: int, width: int, conv_kernel_size: int, pool_kernel_size: int
+    ) -> tuple[int, int]:
         height = self._compute_dim(height, conv_kernel_size)
         width = self._compute_dim(width, conv_kernel_size)
 
-        height = self._compute_dim(height,
-                                   pool_kernel_size,
-                                   stride=pool_kernel_size)
-        width = self._compute_dim(width,
-                                  pool_kernel_size,
-                                  stride=pool_kernel_size)
+        height = self._compute_dim(height, pool_kernel_size, stride=pool_kernel_size)
+        width = self._compute_dim(width, pool_kernel_size, stride=pool_kernel_size)
 
         return (height, width)
--- src/template_nn/networks/fnn.py
+++ src/template_nn/networks/fnn.py
@@ -1,47 +1,41 @@
-from typing import List
-
-import pandas as pd
 import torch.nn as nn
 
 from .base_nn import BaseNetwork
 from ..retrieve_keys import get_model_keys
-from ..args_val import (is_positive_int, is_iterable, has_activation_functions,
-                        activation_functions_check)
+from ..args_val import (
+    is_positive_int,
+    is_iterable,
+    has_activation_functions,
+    activation_functions_check,
+)
 
 
 class FNN(BaseNetwork):
     """
     A Feedforward Neural Network (FNN) model for supervised learning.
-
-    References:
-        - Suganthan, P. N., & Katuwal, R. (2021). On the origins of randomization-based feedforward neural networks.
-          *Applied Soft Computing*, 105, 107239. [DOI: 10.1016/j.asoc.2021.107239](https://doi.org/10.1016/j.asoc.2021.107239)
-
     """
 
-    def __init__(self,
-                 tabular: dict | pd.DataFrame,
-                 visualise: bool = False) -> None:
+    def __init__(
+        self,
+        model_config: dict[str, int | list[int] | list[str]],
+        visualise: bool = False,
+    ) -> None:
         """
-        Class description goes here
-        :param tabular: An input accepting both a dictionary or a pandas.DataFrame object.
-        :param visualise: A toggle switch to visualize the model. OFF(False) by default.
+        :params model_config: A dictionary / json-like structure for model configuration
+        :params visualise: A boolean type for visualising structure. Default (False).
         """
+        super().__init__(visualise)
+        self.model_keys = get_model_keys("FNN")
+        self.params = self._get_params(model_config, self.model_keys)
+        self.model = self._build_model(*self.params)
 
-        super().__init__(tabular, get_model_keys("FNN"), visualise)
-
-    def _build_model(self, input_size: int, output_size: int,
-                     hidden_sizes: List[int],
-                     activation_functions: List[nn.Module]) -> nn.Sequential:
-        """
-        A procedural function declaring the steps of building a model.
-        :param input_size: The number of input features.
-        :param output_size: The number of output features.
-        :param hidden_sizes: The number of nodes in each hidden layer.
-        :param activation_functions: The activation functions to use.
-        :return: A torch.nn.Sequential object representing the layers.
-        """
-
+    def _build_model(
+        self,
+        input_size: int,
+        output_size: int,
+        hidden_sizes: list[int],
+        activation_functions: list[str],
+    ) -> nn.Sequential:
         is_positive_int(input_size)
         is_positive_int(output_size)
         is_iterable(hidden_sizes)
@@ -52,32 +46,25 @@ class FNN(BaseNetwork):
         has_activation_functions(activation_functions)
         activation_functions_check(activation_functions, hidden_sizes)
 
-        return nn.Sequential(*self._create_layers(
-            input_size, output_size, hidden_sizes, activation_functions))
+        return nn.Sequential(
+            *self._create_layers(
+                input_size, output_size, hidden_sizes, activation_functions
+            )
+        )
 
     def _create_layers(
-            self, input_size: int, output_size: int, hidden_sizes: List[int],
-            activation_functions: List[nn.Module]) -> list[nn.Module]:
-        """
-        A function to generate layers dynamically.
-        :param input_size: The number of input features.
-        :param hidden_sizes: The number of nodes in each hidden layer.
-        :param output_size: The number of output features.
-        :param activation_functions: The activation functions to use.
-        :return: A list of torch.nn activation functions. Refer to the official documentation for possible inputs:
-        https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity and
-        https://pytorch.org/docs/stable/nn.html#non-linear-activations-other
-        """
-
-        layers = []
-
+        self,
+        input_size: int,
+        output_size: int,
+        hidden_sizes: list[int],
+        activation_functions: list[str],
+    ) -> list[nn.Module]:
+        layers: list[nn.Module] = []
         in_size = input_size
 
-        for hidden_size, activation_function in zip(hidden_sizes,
-                                                    activation_functions):
-            layers += [nn.Linear(in_size, hidden_size), activation_function]
-
-            # go to next layer
+        for hidden_size, activation_function in zip(hidden_sizes, activation_functions):
+            layers.append(nn.Linear(in_size, hidden_size))
+            layers.append(getattr(nn, activation_function)())
             in_size = hidden_size
 
         layers.append(nn.Linear(hidden_sizes[-1], output_size))
